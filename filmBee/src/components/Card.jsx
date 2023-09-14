e96a9acbd08fc591de4cee7ed5a14893fcb396e1
Md Rashed Ali <tools.rashed@gmail.com>
2023-09-14 13:06:03 +0600

    card content added

--- filmBee/src/components/Card.jsx
+++ filmBee/src/components/Card.jsx
@@ -1,9 +1,16 @@
 import PropTypes from "prop-types";
 const Cart = ({ singleData }) => {
   return (
-    <div className="border-2 border-red-200 p-3">
-      <img src={singleData.image} alt="" />
-      <h1>{singleData.name}</h1>
+    <div className="border-2 border-red-200 p-3 rounded flex flex-col justify-center items-center text-center">
+      <img
+        className="h-52 w-44 object-cover object-center"
+        src={singleData.image}
+        alt=""
+      />
+      <h1>Name: {singleData.name}</h1>
+      <h1>Title: {singleData.title}</h1>
+      <h1>Salary: {singleData.salary}</h1>
+      <button className="bg-red-200 p-2 rounded">Select</button>
     </div>
   );
 };
